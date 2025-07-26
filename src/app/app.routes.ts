3aea13e596f2fa8b696e3c8dd9c793f1736502e3
Nelson Molina <geovannysangucho25@gmail.com>
2025-07-26 15:30:26 -0500

    Deploy Angular 20 production build clean

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile';
-
-
-
-export const routes: Routes = [
-  { path: '', component: ProfileComponent },
-  { path: 'home', component: ProfileComponent }
-];
